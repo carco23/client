d17f6a98840b05181374c3b9869190c27c5bd7c9
carcika23 <130472163+carcika23@users.noreply.github.com>
2024-11-14 00:42:27 +0400

    aboutUsFix

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,10 +2,43 @@ import React from 'react';
 import styles from './About.module.css'
 import Zurab from '../../assets/zurab.svg'
 import Ilia from '../../assets/ilia.svg'
-
-const text = ''
+import { useState, useEffect } from 'react';
+import { Carousel, Card, Button } from 'antd';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import './slide.css'
 
 function About() {
+
+  const slides = [
+    [
+    {
+      name: 'Zurab Azarashvili',
+      position: 'Managing Partner',
+      bio: 'Zurab Azarashvili is a Georgian public figure who served as Minister of the Ministry of the Internally Displaced Persons from the Occupied Territories, Labor, Health, and Social Affairs of Georgia and as the Deputy Minister of the Ministry of Defense of Georgia - 2021-2024 He has 20+ years of banking experience in various leading banks in Georgia at Top Management positions.',
+      img: Zurab
+    },
+    {
+      name: 'Ilia Gudushauri',
+      position: 'Partner- Advisory Services',
+      bio: 'Ilia Gudushauri is a consulting advisory professional who served as Deputy Minister of the Ministry of the Internally Displaced Persons from the Occupied Territories, Labor, Health, and Social Affairs of Georgia 2022-2024 He has 10+ years of financial experience in Deloitte in Georgia at Top Management position.',
+      img: Ilia
+    }
+  ]
+  ]
+
+  const carouselRef = React.useRef(null);
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 768);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const nextSlide = () => carouselRef.current.next();
+  const prevSlide = () => carouselRef.current.prev();
+
   return (
     <div className={styles.aboutUs} id='aboutUs'>
       <div className={styles.text}>
@@ -18,6 +51,40 @@ function About() {
         <br/>
         These are effective policies that are having immediate, tangible impact on financial protection for Georgian population and this generates optimism for us in WHO as we seek to support other Member States in these areas.</p>
         </div>
+        {isMobile ? (
+          <div className="antd-slider">
+          {/* <Button className="arrow-button" onClick={prevSlide} icon={<LeftOutlined />} /> */}
+    
+            <Carousel
+            ref={carouselRef}
+        dots={true}         // Show navigation dots
+        infinite={true}     // Prevent infinite looping (optional)
+        slidesToShow={1}     // Show one card at a time
+        slidesToScroll={1}   // Scroll one card at a time
+      >
+        <div className={styles.personCard}>
+            <img src={Zurab} />
+            <div className={styles.name}>
+              <h1>Zurab Azarashvili</h1>
+              <p>Managing Partner</p>
+            </div>
+            <p className={styles.bio}>Zurab Azarashvili is a Georgian public figure who served as Minister of the Ministry of the Internally Displaced Persons from the Occupied Territories, Labor, Health, and Social Affairs of Georgia and as the Deputy Minister of the Ministry of Defense of Georgia - 2021-2024 He has 20+ years of banking experience in various leading banks in Georgia at Top Management positions.</p>
+          </div>
+          <div className={styles.personCard}>
+            <img src={Ilia} />
+            <div className={styles.name}>
+              <h1>Ilia Gudushauri</h1>
+              <p>Partner- Advisory Services</p>
+            </div>
+            <p className={styles.bio}>Ilia Gudushauri is a consulting advisory professional who served as Deputy Minister of the Ministry of the Internally Displaced Persons from the Occupied Territories, Labor, Health, and Social Affairs of Georgia 2022-2024 He has 10+ years of financial experience in Deloitte in Georgia at Top Management position.</p>
+          </div>
+          
+        {/* Add more cards as needed */}
+    </Carousel>
+    
+          {/* <Button className="arrow-button" onClick={nextSlide} icon={<RightOutlined />} /> */}
+          </div>
+        ) : (
         <div className={styles.personCards}>
           <div className={styles.personCard}>
             <img src={Zurab} />
@@ -36,6 +103,7 @@ function About() {
             <p className={styles.bio}>Ilia Gudushauri is a consulting advisory professional who served as Deputy Minister of the Ministry of the Internally Displaced Persons from the Occupied Territories, Labor, Health, and Social Affairs of Georgia 2022-2024 He has 10+ years of financial experience in Deloitte in Georgia at Top Management position.</p>
           </div>
         </div>
+        )}
         <button>Learn More</button>
     </div>
   );
